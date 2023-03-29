dcce68b97de28f9f25486c3accb4f14e888f587c
kantoash <kentoashketchum@gmail.com>
2023-03-30 03:27:38 +0530

    second

--- components/PreviewPage.tsx
+++ components/PreviewPage.tsx
@@ -2,10 +2,9 @@
 
 import { groq } from "next-sanity";
 import { usePreview } from "../lib/sanity.preview";
-import { PageInfo, Project, Skill, Social } from "../typing";
+import { PageInfo, Project, Social } from "../typing";
 import About from "./About";
 import ContactMe from "./ContactMe";
-import Header from "./Header";
 import Hero from "./Hero";
 import Projects from "./Projects";
 import Skills from "./Skills";
@@ -25,27 +24,21 @@ const projectQuery = groq`
   }
 `;
 
-const skillQuery = groq`
- *[_type == "skill"]
-`;
-
 export default function PreviewPage() {
   const pageInfo: PageInfo = usePreview(null, pageInfoQuery);
   const socials: Social[] = usePreview(null, socialsQuery);
   const projects: Project[] = usePreview(null, projectQuery);
-  const skills: Skill[] = usePreview(null, skillQuery);
 
   return (
     <div className="bg-[rgb(41,41,41)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-      <Header socials={socials} />
       <section id="hero" className="snap-start">
-        <Hero pageInfo={pageInfo} />
+        <Hero pageInfo={pageInfo} socials={socials} />
       </section>
       <section id="About" className="snap-center">
         <About pageInfo={pageInfo} />
       </section>
       <section id="skills" className="snap-start">
-        <Skills skills={skills} />
+        <Skills/>
       </section>
       <section id="projects" className="snap-start">
         <Projects projects={projects} />
